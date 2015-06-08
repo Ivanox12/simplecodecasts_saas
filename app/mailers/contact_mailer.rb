a3892f04986a570b7fbef38e15b24bac2f38366e
Ivan Quevedo <ivanox12@hotmail.com>
2015-06-08 21:21:17 +0000

    fix misspelled validation on contact.rb and contact_mailer.rb update

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,8 @@
-class ContactMailer < ActionMailer :: Base
+class ContactMailer < ActionMailer::Base
+  default to:'ivan.quevedo.garcia@gmail.com'
+  @name = name
+  @email = email
+  @body = body
+  
+  mail(from: email, subject: 'Contact form Message')
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-  validates :name, presence: ture
+  validates :name, presence: true
   
 
 end
