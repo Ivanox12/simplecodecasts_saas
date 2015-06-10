f5411a69accf3f791fafd02522c206642c53bcea
Ivan Quevedo <ivanox12@hotmail.com>
2015-06-10 21:11:41 +0000

    Fix typo's on users.js line 8 to 11

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccNum = $('#card_number').vall(),
-        cvcNum =$('#card_code').vall(),
-        expMonth = $('#card_month').vall(),
-        expYear =$('#card_year').vall();
+    var ccNum = $('#card_number').val(),
+        cvcNum =$('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear =$('#card_year').val();
     
     if (!error){
       //Get the stripe token:
