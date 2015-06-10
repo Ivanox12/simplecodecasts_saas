f797c14cce75cbc4c8b7f3dd861de13d97eb571a
Ivan Quevedo <ivanox12@hotmail.com>
2015-06-10 19:15:31 +0000

    Modified devise for stripe

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  //Watch for a form submission:
+  $("#form-submit-btn").click(function(event){
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').vall(),
+        cvcNum =$('#card_code').vall(),
+        expMonth = $('#card_month').vall(),
+        expYear =$('#card_year').vall();
+    
+    if (!error){
+      //Get the stripe token:
+       Stripe.createToken({
+         number: ccNum,
+         cvc: cvcNum,
+         exp_month: expMonth,
+         exp_year: expYear
+       },striperesponseHandler);
+    }
+    return false;
+  }); // form submission
+  
+  function stripeResponseHandler(status, response){
+    // Get reference to the form 
+    var f = $("#new user");
+    
+    // Get the token from the response:
+    var token = response.id;
+    
+    //Add the token to the form:
+    f.append('<input type="hidden" name= "user[stripe_card_token]" value" ' + token + '"/>');
+    
+    // Submit the form 
+    f.get(0).submit();
+  }
+});gi
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  
+  def create
+    super do |resource|
+      if params[:plans]
+        resource.plan_id = params[:plan]
+        if resources.plan_id == 2
+          resources.save_with_payment
+        else
+          resource.save
+  end
+  
+  
+  
+end
