a56b73428928ff560a50fac384b7d603f2631058
Ivan Quevedo <ivanox12@hotmail.com>
2015-06-11 21:16:59 +0000

    fix typo in line 19 of the registration_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,7 +16,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   
   private 
   def select_plan
-    unless params[:params] && (params[:plan] == '1' || params[:plan] == '2')
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
       flash[:notice] = "Please select a mebership plan to sing up."
       redirect_to root_url
     end
